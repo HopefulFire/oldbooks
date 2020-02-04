45b21503a04ec3043b076cd3c8d4ed7697046970
Jade Lynn Masker <hopefulfire@gmail.com>
2020-02-03 20:38:05 -0500

    removed unnecessary puts

--- lib/oldbooks/scraper.rb
+++ lib/oldbooks/scraper.rb
@@ -8,13 +8,13 @@ class Oldbooks::Scraper
 
 	def scrape_author_and_books
 		doc = Nokogiri::HTML(open(@url))
-		puts name = doc.css('h2').text
-		puts bio = doc.css('#biog').text
-		puts gender = bio.split('Nationality: ')[0].gsub('Gender : ', '').strip
-		puts nationality = bio.split('Born : ')[0].split('Nationality: ')[1]
-		puts born = bio.split('Lived :  ')[0].split('Born : ')[1].split(' ')[0]
-		puts died = bio.split('aged : ')[0].split('Died : ')[1].split(' ')[0]
-		puts age = bio.split('aged : ')[1].split("\n")[0]
+		name = doc.css('h2').text
+		bio = doc.css('#biog').text
+		gender = bio.split('Nationality: ')[0].gsub('Gender : ', '').strip
+		nationality = bio.split('Born : ')[0].split('Nationality: ')[1]
+		born = bio.split('Lived :  ')[0].split('Born : ')[1].split(' ')[0]
+		died = bio.split('aged : ')[0].split('Died : ')[1].split(' ')[0]
+		age = bio.split('aged : ')[1].split("\n")[0]
 	end
 
 end
