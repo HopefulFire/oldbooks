b7f910cd58476c72cd9ad028156da255f54486f5
Jade Lynn Masker <hopefulfire@gmail.com>
2020-02-03 20:42:56 -0500

    added spaces in #scrape_author_and_books

--- lib/oldbooks/scraper.rb
+++ lib/oldbooks/scraper.rb
@@ -8,13 +8,20 @@ class Oldbooks::Scraper
 
 	def scrape_author_and_books
 		doc = Nokogiri::HTML(open(@url))
+
 		name = doc.css('h2').text
+
 		bio = doc.css('#biog').text
+		
 		gender = bio.split('Nationality: ')[0].gsub('Gender : ', '').strip
 		nationality = bio.split('Born : ')[0].split('Nationality: ')[1]
 		born = bio.split('Lived :  ')[0].split('Born : ')[1].split(' ')[0]
 		died = bio.split('aged : ')[0].split('Died : ')[1].split(' ')[0]
 		age = bio.split('aged : ')[1].split("\n")[0]
+
+		author = Oldbooks::Author.create(name: name, gender: gender,
+			nationality: nationality, born: born, died: died, age: age)
+		
 	end
 
 end
