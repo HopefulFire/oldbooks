727a79f71a01843016241192540cce3ed7ef8b27
Jade Lynn Masker <hopefulfire@gmail.com>
2020-02-03 20:36:39 -0500

    finished scraping author

--- lib/oldbooks/scraper.rb
+++ lib/oldbooks/scraper.rb
@@ -8,11 +8,13 @@ class Oldbooks::Scraper
 
 	def scrape_author_and_books
 		doc = Nokogiri::HTML(open(@url))
-		name = doc.css('h2').text
+		puts name = doc.css('h2').text
 		puts bio = doc.css('#biog').text
 		puts gender = bio.split('Nationality: ')[0].gsub('Gender : ', '').strip
 		puts nationality = bio.split('Born : ')[0].split('Nationality: ')[1]
-		puts born = bio.split('Lived :  ')[0].split('Born : ')[1].split(' ')[0] #not splitting on space!
+		puts born = bio.split('Lived :  ')[0].split('Born : ')[1].split(' ')[0]
+		puts died = bio.split('aged : ')[0].split('Died : ')[1].split(' ')[0]
+		puts age = bio.split('aged : ')[1].split("\n")[0]
 	end
 
 end
