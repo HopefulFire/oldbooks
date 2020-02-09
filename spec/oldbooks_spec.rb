a8932583c1ca26c9fa5d977e4547e3129d2b31e8
Jade Lynn Masker <hopefulfire@gmail.com>
2020-02-08 19:41:25 -0500

    removed book class for rewrite

--- spec/oldbooks_spec.rb
+++ spec/oldbooks_spec.rb
@@ -7,28 +7,6 @@ RSpec.describe Oldbooks do
 		expect(true).to eq(true)
 	end
 
-	context '::Book' do
-		it 'initializes with a title, an author, an info, a date, and an url' do
-			book = Oldbooks::Book.new(title: 'title', author: 'author', info: 'info', date: 'date', url: "url")
-			expect(book.title).to eq('title')
-			expect(book.author).to eq('author')
-			expect(book.info).to eq('info')
-			expect(book.date).to eq('date')
-			expect(book.url).to eq('url')
-		end
-		it '.create initializes a book and saves it' do
-			book1 = Oldbooks::Book.create
-			book2 = Oldbooks::Book.create
-			expect(Oldbooks::Book::ALL).to include(book1)
-			expect(Oldbooks::Book::ALL).to include(book2)
-		end
-		it '.clear_all deletes all books' do
-			book = Oldbooks::Book.create
-			Oldbooks::Book.clear_all
-			expect(Oldbooks::Book::ALL).to eq([])
-		end
-	end
-
 	context '::Scraper' do
 		it 'initializes with a url' do
 			scraper = Oldbooks::Scraper.new('https://duckduckgo.com/')
