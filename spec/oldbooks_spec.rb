48c220a816f52c574224b34b0eb27d5327c36904
Jade Lynn Masker <hopefulfire@gmail.com>
2020-02-08 19:38:20 -0500

    removed unnecessary author class

--- spec/oldbooks_spec.rb
+++ spec/oldbooks_spec.rb
@@ -29,39 +29,6 @@ RSpec.describe Oldbooks do
 		end
 	end
 
-	context '::Author' do
-		it 'initializes with a name, gender, nationality, born, died, and age' do
-			author = Oldbooks::Author.new(name: 'name', gender: 'gender', nationality: 'nationality', born: 'born', died: 'died', age: 'age')
-			expect(author.name).to eq('name')
-			expect(author.gender).to eq('gender')
-			expect(author.nationality).to eq('nationality')
-			expect(author.born).to eq('born')			
-			expect(author.died).to eq('died')
-			expect(author.age).to eq('age')
-		end
-		it 'has_many books' do
-			author = Oldbooks::Author.new
-			expect(author.books.class).to be(Array)
-		end
-		it '#add_book can add a book' do
-			author = Oldbooks::Author.new
-			book = Oldbooks::Book.new
-			author.add_book(book)
-			expect(author.books).to include(book)
-		end
-		it '.create initializes an author and saves it' do
-			author1 = Oldbooks::Author.create
-			author2 = Oldbooks::Author.create
-			expect(Oldbooks::Author::ALL).to include(author1)
-			expect(Oldbooks::Author::ALL).to include(author2)
-		end
-		it '.clear_all deletes all authors' do
-			author = Oldbooks::Author.create
-			Oldbooks::Author.clear_all
-			expect(Oldbooks::Author::ALL).to eq([])
-		end
-	end
-
 	context '::Scraper' do
 		it 'initializes with a url' do
 			scraper = Oldbooks::Scraper.new('https://duckduckgo.com/')
